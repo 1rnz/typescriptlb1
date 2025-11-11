fa38f599c1d1f418603e55ca2a1c69222c08ff04
Lorenz <lorentsvlad@gmail.com>
2025-11-11 19:15:10 +0200

    Додано базову структуру TypeScript проєкту

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = "Олександр";
+let userAge = 25;
+let isDeveloper = true;
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${userAge}`);
+console.log(`Розробник: ${isDeveloper ? "так" : "ні"}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let userName: string = "Олександр";
+let userAge: number = 25;
+let isDeveloper: boolean = true;
+
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${userAge}`);
+console.log(`Розробник: ${isDeveloper ? "так" : "ні"}`);
